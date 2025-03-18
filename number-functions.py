930d6fe13a2f916920f6694d0c17f4088ecf2a78
Wayette <dylanngara@gmail.com>
2025-03-18 10:45:53 +0300

    Python basics

--- number-functions.py
+++ number-functions.py
@@ -6,3 +6,18 @@ print(math.sqrt(x))
 print(math.pi)
 print(math.floor(78.88))
 print(math.ceil(78.88))
+
+
+radius = 54
+height = 78
+
+volume = 22/7 * radius * radius * height
+volume = round(volume, 2)
+print(volume)
+
+sa = ( 2 * 22/7 * radius ) + ( 22/7 * (radius + radius)* height )
+sa = round(sa, 2)
+print(sa)
+
+result = 10 + 20 / 5 * 6 - 3 * (5 + 2)
+print(result)
